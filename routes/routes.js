ffe6c3547e89707fe70251edb9162aad3103632e
TheoWebDev <theo.demarle@live.fr>
2022-09-25 11:53:33 +0200

    add new item schema user

--- routes/routes.js
+++ routes/routes.js
@@ -635,6 +635,8 @@ router
                         company_u: user.company_u,
                         phone_u: user.phone_u,
                         address_u: user.address_u,
+                        zipCode_u: user.zipCode_u,
+                        country_u: user.country_u,
                         siret_u: user.siret_u
                     }
                     const token = jwt.sign({ user: body }, process.env.JWT_SECRET, { expiresIn: '30d' })
